7715bb37a39b2ba97bb1027920262744d56cbf01
s26834-pj <115928176+s26834-pj@users.noreply.github.com>
2022-11-18 18:41:32 +0100

    Add files via upload

---
+++ Zadanie3.2.cpp
@@ -0,0 +1,19 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    char a;
+    
+    do
+    {
+        cout << "Wprowadz znak ";
+        cin >> a;
+    } 
+    while (a != 't');
+    
+    cout << endl << "Wprowadziles znak 't'. Program zakonczyl prace." << endl;
+
+    return 0;
+}
